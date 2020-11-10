b9f5a546fc88f9e5a5144da4a515cf904a995555
SebG-prog <sebastien-gruet@orange.fr>
2020-11-10 07:44:34 +0100

    npm run lint-fix, 10 problems left

--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,44 @@
 // index.js
 const asyncHandler = require('express-async-handler');
-const createError = require('http-errors')
+const createError = require('http-errors');
 const express = require('express');
-const mongoose = require("mongoose")
-const wildController = require("./controllers/wilders");
+const mongoose = require('mongoose');
+const wildController = require('./controllers/wilders');
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/wilderdb", {
+mongoose.connect('mongodb://127.0.0.1:27017/wilderdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  autoIndex: true
+  autoIndex: true,
 })
-  .then(() => console.log("Connected to DB"))
+  .then(() => console.log('Connected to DB'))
   .catch((err) => console.log(err));
 
-app.post("/api/wilders", asyncHandler(wildController.create))
+app.post('/api/wilders', asyncHandler(wildController.create));
 
-app.get("/api/wilders", asyncHandler(wildController.retrieve))
+app.get('/api/wilders', asyncHandler(wildController.retrieve));
 
-app.put("/api/wilder/:_id", asyncHandler(wildController.update))
+app.put('/api/wilder/:_id', asyncHandler(wildController.update));
 
-app.delete("/api/wilder/:_id", asyncHandler(wildController.delete))
+app.delete('/api/wilder/:_id', asyncHandler(wildController.delete));
 
 app.use((req, res, next) => {
-  next(createError(404, "There is nothing there"))
-})
+  next(createError(404, 'There is nothing there'));
+});
 
 app.use((error, req, res, next) => {
   if (error.name === 'MongoError' || error.code === 11000) {
-    res.status(400)
-    error.message =  "Named already used in DB"
+    res.status(400);
+    error.message = 'Named already used in DB';
   }
 
-  res.status(error.status || 500)
-  res.json({ success: false, message: error.message, stack: error.stack})
-})
+  res.status(error.status || 500);
+  res.json({ success: false, message: error.message, stack: error.stack });
+});
 
-app.listen(8000, () => console.log("Server started on port 8000"))
+app.listen(8000, () => console.log('Server started on port 8000'));
